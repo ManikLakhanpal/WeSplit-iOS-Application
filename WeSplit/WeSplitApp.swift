128bd98665642d77ba43006e7796086cd85e216e
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-05-19 11:55:08 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Manik Lakhanpal on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
